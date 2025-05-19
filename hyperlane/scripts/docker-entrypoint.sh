f2edb5263619af7bcb2425a444d5ca130f610d7c
Damian Nolan <damiannolan@gmail.com>
2025-05-19 18:55:37 +0200

    feat: add dockerfile and entrypoint script to deploy hyperlane

---
+++ hyperlane/scripts/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export HYP_KEY=0x82bfcfadbf1712f6550d8d2c00a39f05b33ec78939d0167be2a737d691f33a6a
+
+echo "Using Hyperlane registry:"
+hyperlane registry list --registry ./hyperlane
+
+echo "Deploying Hyperlane core EVM contracts..."
+hyperlane core deploy --chain rethlocal --registry ./hyperlane --yes
+
+echo "Deploying Hyperlane warp synthetic token EVM contracts..."
+hyperlane warp deploy --config ./configs/warp-config.yaml --registry ./hyperlane --yes
+
+echo "Deploying Hyperlane on cosmosnative..."
+hyp deploy celestia-validator:9090
