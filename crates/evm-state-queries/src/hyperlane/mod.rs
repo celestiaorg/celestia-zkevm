eb7d6bab1fabd269c7704c1b30e1491d66fcabdd
jonas pauli <jonaspauli089@gmail.com>
2025-08-31 01:31:40 +0200

    add comment to test_run_indexer

--- crates/evm-state-queries/src/hyperlane/mod.rs
+++ crates/evm-state-queries/src/hyperlane/mod.rs
@@ -17,6 +17,18 @@ mod tests {
     }
 
     #[tokio::test]
+    /* Context
+        We want to generate proofs for events that occurred between one finalized block and another (latest)
+        finalized block. Therefore we can query the relevant events using the filter and insert them into the tree,
+        starting at a previous checkpoint (or the empty tree).
+
+        When using the indexer we must ensure that we start indexing from the first block that includes a message to
+        be able to replay all inserts into the tree and obtain the correct branch that exists on-chain.
+
+        A storage proof to that branch will be included and verified inside the circuit against the root of said (latest)
+        finalized block. It is advisible to maintain a window / root history on-chain so that this proof will verify even
+        if a new block was posted in the meantime.
+    */
     async fn test_run_indexer() {
         let indexer = HyperlaneIndexer::default();
         let message_store = Arc::new(HyperlaneMessageStore::from_env().unwrap());
