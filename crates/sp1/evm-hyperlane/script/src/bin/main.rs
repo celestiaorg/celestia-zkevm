a5c246484746daad177505e3853d02e284446289
jonas pauli <jonaspauli089@gmail.com>
2025-09-09 12:35:23 +0200

    write proof inputs fn

--- crates/sp1/evm-hyperlane/script/src/bin/main.rs
+++ crates/sp1/evm-hyperlane/script/src/bin/main.rs
@@ -10,10 +10,9 @@
 //! RUST_LOG=info cargo run -p evm-hyperlane-script --release -- --prove --contract 0xFCb1d485ef46344029D9E8A7925925e146B3430E --start-idx 0 --end-idx 23 --target-height 268 --rpc-url http://127.0.0.1:8545
 //! ```
 
-use std::{str::FromStr, time::Instant};
-
 use alloy_primitives::Address;
 use alloy_provider::ProviderBuilder;
+use anyhow::Result;
 use clap::{Parser, command};
 use evm_hyperlane_types_sp1::{HyperlaneMessageInputs, HyperlaneMessageOutputs, tree::MerkleTree};
 use evm_storage_proofs::{
@@ -21,6 +20,7 @@ use evm_storage_proofs::{
     types::{HYPERLANE_MERKLE_TREE_KEYS, HyperlaneBranchProof, HyperlaneBranchProofInputs},
 };
 use sp1_sdk::{ProverClient, SP1Stdin, include_elf};
+use std::{str::FromStr, time::Instant};
 use storage::{Storage, hyperlane_messages::storage::HyperlaneMessageStore};
 use url::Url;
 
@@ -72,6 +72,32 @@ async fn main() {
 
     // Setup the inputs.
     let mut stdin = SP1Stdin::new();
+    write_proof_inputs(&mut stdin, &args)
+        .await
+        .expect("failed to write proof inputs");
+
+    if args.execute {
+        client.execute(EVM_HYPERLANE_ELF, &stdin).run().unwrap();
+        println!("Program executed successfully!");
+    } else {
+        // Setup the program for proving.
+        let (pk, vk) = client.setup(EVM_HYPERLANE_ELF);
+        let start_time = Instant::now();
+        // Generate the proof
+        let proof = client.prove(&pk, &stdin).run().expect("failed to generate proof");
+        println!("Proof generation time: {:?}", Instant::now() - start_time);
+        println!("Successfully generated proof!");
+
+        // Verify the proof.
+        client.verify(&proof, &vk).expect("failed to verify proof");
+        println!("Successfully verified proof!");
+
+        let proof_outputs: HyperlaneMessageOutputs = bincode::deserialize(proof.public_values.as_slice()).unwrap();
+        println!("Proof outputs: {proof_outputs:?}");
+    }
+}
+
+async fn write_proof_inputs(stdin: &mut SP1Stdin, args: &Args) -> Result<()> {
     let message_db = HyperlaneMessageStore::from_env().unwrap();
     let mut messages = Vec::new();
     for height in args.start_idx..=args.end_idx {
@@ -95,30 +121,11 @@ async fn main() {
 
     let inputs = HyperlaneMessageInputs::new(
         execution_state_root,
-        args.contract,
+        args.contract.clone(),
         messages.into_iter().map(|m| m.message).collect(),
         HyperlaneBranchProofInputs::from(branch_proof),
         MerkleTree::default(),
     );
     stdin.write(&inputs);
-
-    if args.execute {
-        client.execute(EVM_HYPERLANE_ELF, &stdin).run().unwrap();
-        println!("Program executed successfully!");
-    } else {
-        // Setup the program for proving.
-        let (pk, vk) = client.setup(EVM_HYPERLANE_ELF);
-        let start_time = Instant::now();
-        // Generate the proof
-        let proof = client.prove(&pk, &stdin).run().expect("failed to generate proof");
-        println!("Proof generation time: {:?}", Instant::now() - start_time);
-        println!("Successfully generated proof!");
-
-        // Verify the proof.
-        client.verify(&proof, &vk).expect("failed to verify proof");
-        println!("Successfully verified proof!");
-
-        let proof_outputs: HyperlaneMessageOutputs = bincode::deserialize(proof.public_values.as_slice()).unwrap();
-        println!("Proof outputs: {proof_outputs:?}");
-    }
+    Ok(())
 }
