8e796f52b115bf09961f1b3951b0ef4be1e2d810
Damian Nolan <damiannolan@gmail.com>
2025-09-16 10:02:43 +0200

    chore: disallow absence proofs in block prover program

--- crates/sp1/evm-exec/program/src/main.rs
+++ crates/sp1/evm-exec/program/src/main.rs
@@ -106,6 +106,8 @@ pub fn main() {
 
     let mut cursor = 0;
     for (proof, root) in inputs.proofs.iter().zip(roots) {
+        assert!(proof.is_of_presence(), "NamespaceProof must be of presence");
+
         let share_count = (proof.end_idx() - proof.start_idx()) as usize;
         let end = cursor + share_count;
 
