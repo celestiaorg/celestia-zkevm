93531c68efc16c8e100d422b4999363821755918
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2025-10-02 14:20:29 +0200

    Feat: Initial E2E with Block and Message Proof Submission to ZKISM (#221)
    * merge changes from jonas/experimental and import types from celestia-grpc-client
    
    * fix merge conflicts
    
    * remove unused deps
    
    * fix use correct target height in e2e
    
    * warnings
    
    * update zism id

--- crates/e2e/src/bin/message.rs
+++ crates/e2e/src/bin/message.rs
@@ -4,17 +4,20 @@ use e2e::{
     prover::message::prove_messages,
 };
 use ev_state_queries::MockStateQueryProvider;
-use std::str::FromStr;
+use sp1_sdk::{EnvProver, ProverClient};
+use std::{str::FromStr, sync::Arc};
 use url::Url;
 
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
+    let client: Arc<EnvProver> = Arc::new(ProverClient::from_env());
     let evm_provider = ProviderBuilder::new().connect_http(Url::from_str(EV_RPC).unwrap());
     let _proof = prove_messages(
         TARGET_HEIGHT,
         &evm_provider.clone(),
         &MockStateQueryProvider::new(evm_provider),
+        client,
     )
     .await
     .unwrap();
