6ac9738bcb4f383e62ffa4a84e3f77e69dc5d0b6
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2025-10-16 11:22:13 +0200

    Enhance: Fully automated E2E with transfer and transfer-back (#240)
    * transfer-back in e2e
    
    * automatically transfer and transfer-back
    
    * clippy
    
    * update readme
    
    * add another timeout
    
    * add tracing
    
    * add info/debug mode to readme
    
    * increase timeout for now
    
    * add comment on timeout
    
    * fix typo
    
    * add retry timeout
    
    * remove comment
    
    * cleanup
    
    * update readme
    
    * add e2e instructions to readme
    
    * clippy

--- README.md
+++ README.md
@@ -80,6 +80,56 @@ NETWORK_PRIVATE_KEY="PRIVATE_KEY" to the SP1 prover network private key from Cel
     make stop
     ```
 
+## Running the E2E test
+1. Clone this repository.
+
+    ```shell
+    git clone git@github.com:celestiaorg/celestia-zkevm-hl-testnet.git
+    ```
+
+2. Source the provided `.env` file in this repository.
+
+    ```shell
+    cp .env.example .env
+
+    set -a
+    source .env
+    set +a
+    ```
+
+3. Select a prover mode other than `mock`. Valid choices are `network`, `cuda`, `cpu`.
+    in `.env`:
+    ```shell
+    SP1_PROVER=cpu #network, cuda
+    ```
+
+5. Use the prover service binary to generate config files locally.
+    ```
+    cargo run --bin ev-prover init
+    ```
+    Alternatively, install the client binary and run init:
+    ```
+    cargo install --path ./crates/ev-prover
+    ev-prover init
+    ```
+
+6. Start the docker compose services.
+
+    ```shell
+    # Run `make start` or `docker compose up` from the root of the repository
+    make start 
+    ```
+
+    Wait for all containers to finish their initialization sequence.
+
+7. Run the e2e
+    ```shell
+    RUST_LOG="e2e=info" make e2e
+    ```
+
+    Note that depending on your hardware it can take a while for the e2e to run,
+    as it will prove a series of EVM blocks leading up to a target height, as well as state inclusion of a Hyperlane deposit message at the target height.
+
 ## Architecture
 
 See [ARCHITECTURE.md](./docs/ARCHITECTURE.md) for more information.
--- crates/e2e/src/bin/e2e.rs
+++ crates/e2e/src/bin/e2e.rs
@@ -1,26 +1,43 @@
 use alloy_primitives::{FixedBytes, hex::FromHex};
 use alloy_provider::ProviderBuilder;
+use celestia_grpc_client::MsgRemoteTransfer;
 use celestia_grpc_client::types::ClientConfig;
 use celestia_grpc_client::{
     MsgProcessMessage, MsgSubmitMessages, MsgUpdateZkExecutionIsm, QueryIsmRequest, client::CelestiaIsmClient,
 };
-use e2e::config::e2e::ISM_ID;
-use e2e::config::other::EV_RPC;
+use e2e::config::debug::EV_RPC;
+use e2e::config::e2e::{CELESTIA_MAILBOX_ID, CELESTIA_TOKEN_ID, EV_RECIPIENT_ADDRESS, ISM_ID};
+use e2e::prover::block::prove_blocks;
+use e2e::prover::helpers::transfer_back;
 use e2e::prover::message::prove_messages;
-use e2e::{config::e2e::TARGET_HEIGHT, prover::block::prove_blocks};
 use ev_state_queries::MockStateQueryProvider;
 use ev_types::v1::{GetMetadataRequest, store_service_client::StoreServiceClient};
 use ev_zkevm_types::hyperlane::encode_hyperlane_message;
 use sp1_sdk::{EnvProver, ProverClient};
+use std::time::Duration;
 use std::{str::FromStr, sync::Arc};
+use tokio::time::sleep;
+use tracing::info;
+use tracing_subscriber::EnvFilter;
 use url::Url;
 
+const MAX_RETRIES: u64 = 10;
+const RETRY_DELAY: u64 = 2;
+
 #[tokio::main]
+#[allow(clippy::field_reassign_with_default)]
 async fn main() {
     rustls::crypto::aws_lc_rs::default_provider()
         .install_default()
         .expect("Failed to set default crypto provider");
     dotenvy::dotenv().ok();
+    let mut filter = EnvFilter::new("sp1_core=warn,sp1_runtime=warn,sp1_sdk=warn,sp1_vm=warn");
+    if let Ok(env_filter) = std::env::var("RUST_LOG") {
+        if let Ok(parsed) = env_filter.parse() {
+            filter = filter.add_directive(parsed);
+        }
+    }
+    tracing_subscriber::fmt().with_env_filter(filter).init();
 
     // instantiate ISM client for submitting payloads and querying state
     let config = ClientConfig::from_env().expect("failed to create celestia client config");
@@ -35,12 +52,67 @@ async fn main() {
     let trusted_root_hex = alloy::hex::encode(ism.state_root);
     let trusted_height = ism.height;
 
+    let transfer_msg = MsgRemoteTransfer::new(
+        ism_client.signer_address().to_string(),
+        CELESTIA_TOKEN_ID.to_string(),
+        1234,
+        EV_RECIPIENT_ADDRESS.to_string(),
+        "1000".to_string(),
+    );
+
+    info!("Bridging Tia from Celestia to Evolve...");
+    let response = ism_client.send_tx(transfer_msg).await.unwrap();
+    assert!(response.success);
+    // we can choose this as our start heihgt, because the state root has not changed in between the hyperlane deployments
+    // and this transfer.
+    let celestia_start_height = response.height - 1;
+
+    info!("Waiting for Evolve balance to be updated...");
+
+    // next trigger make transfer-back
+    info!("Submitting Hyperlane deposit message on Evolve...");
+    let mut retries = 0;
+    let target_height = {
+        loop {
+            let target_height = match transfer_back().await {
+                Ok(height) => height,
+                Err(_) => {
+                    if retries > MAX_RETRIES {
+                        panic!("Failed to get target height after {MAX_RETRIES} retries");
+                    }
+                    sleep(Duration::from_secs(RETRY_DELAY)).await;
+                    retries += 1;
+                    continue;
+                }
+            };
+            break target_height;
+        }
+    };
+    info!("[Done] submitting transfer Messages");
     let client: Arc<EnvProver> = Arc::new(ProverClient::from_env());
-    let target_inclusion_height = inclusion_height(TARGET_HEIGHT).await.unwrap();
-    let start_height = inclusion_height(trusted_height).await.unwrap() + 1;
-    let num_blocks = target_inclusion_height - start_height;
+
+    let mut retries = 0;
+    let target_inclusion_height = {
+        loop {
+            let target_inclusion_height = match inclusion_height(target_height).await {
+                Ok(height) => height,
+                Err(_) => {
+                    if retries > MAX_RETRIES {
+                        panic!("Failed to get target inclusion height after {MAX_RETRIES} retries");
+                    }
+                    sleep(Duration::from_secs(RETRY_DELAY)).await;
+                    retries += 1;
+                    continue;
+                }
+            };
+            break target_inclusion_height;
+        }
+    };
+    let num_blocks = target_inclusion_height - celestia_start_height;
+
+    info!("Proving Evolve blocks...");
     let block_proof = prove_blocks(
-        start_height,
+        celestia_start_height,
         trusted_height,
         num_blocks,
         &mut FixedBytes::from_hex(trusted_root_hex).unwrap(),
@@ -48,6 +120,7 @@ async fn main() {
     )
     .await
     .expect("Failed to prove blocks");
+    info!("[Done] proving blocks");
 
     let block_proof_msg = MsgUpdateZkExecutionIsm::new(
         ISM_ID.to_string(),
@@ -57,12 +130,15 @@ async fn main() {
         ism_client.signer_address().to_string(),
     );
 
+    info!("Updating ZKISM on Celestia...");
     let response = ism_client.send_tx(block_proof_msg).await.unwrap();
     assert!(response.success);
+    info!("[Done] ZKISM was updated successfully");
 
     let evm_provider = ProviderBuilder::new().connect_http(Url::from_str(EV_RPC).unwrap());
+    info!("Proving Evolve Hyperlane deposit events...");
     let message_proof = prove_messages(
-        TARGET_HEIGHT,
+        target_height,
         &evm_provider.clone(),
         &MockStateQueryProvider::new(evm_provider),
         client.clone(),
@@ -72,20 +148,24 @@ async fn main() {
 
     let message_proof_msg = MsgSubmitMessages::new(
         ISM_ID.to_string(),
-        TARGET_HEIGHT,
+        target_height,
         message_proof.0.bytes(),
         message_proof.0.public_values.as_slice().to_vec(),
         ism_client.signer_address().to_string(),
     );
+    info!("[Done] ZKISM was updated successfully");
 
+    info!("Submitting Hyperlane tree proof to ZKISM...");
     let response = ism_client.send_tx(message_proof_msg).await.unwrap();
     assert!(response.success);
+    info!("[Done] ZKISM was updated successfully");
 
+    info!("Relaying verified Hyperlane messages to Celestia...");
     // submit all now verified messages to hyperlane
     for message in message_proof.1 {
         let message_hex = alloy::hex::encode(encode_hyperlane_message(&message.message).unwrap());
         let msg = MsgProcessMessage::new(
-            "0x68797065726c616e650000000000000000000000000000000000000000000000".to_string(),
+            CELESTIA_MAILBOX_ID.to_string(),
             ism_client.signer_address().to_string(),
             alloy::hex::encode(vec![]), // empty metadata; messages are pre-authorized before submission
             message_hex,
@@ -93,6 +173,7 @@ async fn main() {
         let response = ism_client.send_tx(msg).await.unwrap();
         assert!(response.success);
     }
+    info!("[Done] Tia was bridged back to Celestia");
 }
 
 // todo: find a place for this function and remove it from the binaries
@@ -104,6 +185,5 @@ async fn inclusion_height(block_number: u64) -> anyhow::Result<u64> {
 
     let resp = client.get_metadata(req).await?;
     let height = u64::from_le_bytes(resp.into_inner().value[..8].try_into()?);
-
     Ok(height)
 }
