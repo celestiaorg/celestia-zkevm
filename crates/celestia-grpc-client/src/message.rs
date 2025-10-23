03712341cbdb67d48156b2a12ff0afdcbaab74e7
Damian Nolan <damiannolan@gmail.com>
2025-10-23 15:17:46 +0200

    chore: update protos and vkeys

--- crates/celestia-grpc-client/src/message.rs
+++ crates/celestia-grpc-client/src/message.rs
@@ -8,10 +8,9 @@ pub type HyperlaneMessage = MsgProcessMessage;
 
 impl MsgUpdateZkExecutionIsm {
     /// Create a new ZK execution ISM update message
-    pub fn new(id: String, height: u64, proof: Vec<u8>, public_values: Vec<u8>, signer: String) -> Self {
+    pub fn new(id: String, proof: Vec<u8>, public_values: Vec<u8>, signer: String) -> Self {
         Self {
             id,
-            height,
             proof,
             public_values,
             signer,
