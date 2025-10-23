f78ae1fe93ffbfee1bdf608df642afb3d53bd5fa
jonas089 <jonaspauli089@gmail.com>
2025-10-23 17:17:49 +0200

    fmt and sort

--- crates/sp1/ev-combined/script/build.rs
+++
@@ -1,6 +0,0 @@
-use sp1_build::build_program_with_args;
-
-fn main() {
-    build_program_with_args("../program", Default::default());
-    build_program_with_args("../../ev-combined/program", Default::default());
-}
