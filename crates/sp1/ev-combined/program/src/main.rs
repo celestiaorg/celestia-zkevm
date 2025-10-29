335c2fcd643f3b6ade4165491cf726a23c30ce74
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2025-10-29 15:59:33 +0100

    Update main.rs
    Co-authored-by: Damian Nolan <damiannolan@gmail.com>

--- crates/sp1/ev-combined/program/src/main.rs
+++ crates/sp1/ev-combined/program/src/main.rs
@@ -1,18 +1,33 @@
-//! An SP1 program that verifies a sequence of N `ev-exec` proofs.
+//! An SP1 program that verifies the execution and inclusion of ev-reth blocks in
+//! the Celestia data availability network.
 //!
-//! It accepts:
-//! - N verification keys
-//! - N serialized public values (each from a `EvmBlockExecOutput`)
+//! This program combines the functionality contained within `ev-exec` and `ev-range-exec`.
 //!
-//! It performs:
-//! 1. Proof verification for each input
-//! 2. Sequential header verification (i.e., block continuity)
-//! 3. Aggregation of metadata into a `EvmRangeExecOutput`
+//! It accepts N BlockExecInputs which include:
+//! - Celestia block header and associated data availability header (DAH).
+//! - Namespace
+//! - Blobs
+//! - Sequencer Public Key
+//! - NamespaceProofs
+//! - EthClientExecutorInputs (RSP - state transition function)
+//! - Trusted Height
+//! - Trusted State Root
+//!
+//! For each Celestia block it performs the following steps:
+//! 1. Deserializes the program inputs.
+//! 2. Verifies completeness of the namespace using the provided blobs.
+//! 3. Executes the EVM blocks via the state transition function.
+//! 4. Filters blobs to SignedData and verifies the sequencer signature.
+//! 5. Verifies equivalency between the EVM block data and blob data via SignedData.
+//!
+//! The program then verifies sequentiality of each Celestia block output, ensuring a contiguous series.
 //!
 //! It commits:
-//! - The trusted block height and state root
-//! - The new block height and state root
-//! - The latest Celestia header hash from the sequence
+//! - The trusted Celestia block height and header hash.
+//! - The new Celestia block height and header hash.
+//! - The trusted ev-reth block height and state root
+//! - The new ev-reth block height and state root
+//! - The Namespace and public key of the sequencer.
 
 #![no_main]
 sp1_zkvm::entrypoint!(main);
